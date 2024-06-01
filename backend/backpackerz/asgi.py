791a3f7d75ced9da4b9b74fd494e96021ef1f805
ansilrahman777 <rahmanansil777@gmail.com>
2024-06-01 11:38:33 +0530

    asgi

--- backend/backpackerz/asgi.py
+++ backend/backpackerz/asgi.py
@@ -10,8 +10,8 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 import os
 # Initialize Django
 import django
-django.setup()
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backpackerz.settings')
+django.setup()
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
