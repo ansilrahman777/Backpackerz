900f7294315b7356182d43c9cbea0c160f6b784e
ansilrahman777 <rahmanansil777@gmail.com>
2024-06-01 09:33:37 +0530

    static root

--- backend/backpackerz/settings.py
+++ backend/backpackerz/settings.py
@@ -167,7 +167,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT =BASE_DIR /'static'
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = [
     'backpackerz/static'
 ]
