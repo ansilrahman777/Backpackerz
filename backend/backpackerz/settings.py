862f1f6de2d19ef05b279ed7eb3ec252934b8766
ansilrahman777 <rahmanansil777@gmail.com>
2024-06-01 10:23:28 +0530

    static

--- backend/backpackerz/settings.py
+++ backend/backpackerz/settings.py
@@ -170,11 +170,11 @@ STATIC_URL = '/static/'
 import os
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = [
-    'backpackerz/static'
+    '/static'
 ]
 
 MEDIA_URL='media/'
-MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
