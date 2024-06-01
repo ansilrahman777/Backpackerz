9069316a449f67faf921180ed4b1c5b78cb85496
ansilrahman777 <rahmanansil777@gmail.com>
2024-06-01 09:57:39 +0530

    environ error

--- backend/backpackerz/settings.py
+++ backend/backpackerz/settings.py
@@ -18,7 +18,7 @@ from decouple import config
 
 
 
-DEBUG=config('DEBUG')
+# DEBUG=config('DEBUG')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
